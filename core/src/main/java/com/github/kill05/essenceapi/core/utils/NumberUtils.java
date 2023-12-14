11926bb6279cbc3ee3f3d5906f05f30e6d4183de
kill05 <vinkimeme@libero.it>
2023-12-14 13:54:52 +0100

    Improved NumberUtils

--- core/src/main/java/com/github/kill05/essenceapi/core/utils/NumberUtils.java
+++ core/src/main/java/com/github/kill05/essenceapi/core/utils/NumberUtils.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 
 public final class NumberUtils {
 
-    private static final Random random = new Random();
+    public static final Random DEFAULT_RANDOM = new Random();
     private static final int[] romanValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
     private static final String[] romanLiterals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
     private static final String[] numberLiterals = {"", "k", "M", "B", "t", "q", "Q"};
@@ -17,30 +17,43 @@ public final class NumberUtils {
 
     }
 
-    public static boolean isInteger(Object obj){
+    public static boolean isInteger(Object obj) {
+        if(obj instanceof Integer) return true;
+
         try {
             Integer.parseInt(String.valueOf(obj));
             return true;
-        } catch (NullPointerException | NumberFormatException e){
+        } catch (NullPointerException | NumberFormatException e) {
             return false;
         }
     }
 
-    public static boolean isDouble(Object obj){
+    public static boolean isDouble(Object obj) {
+        if(obj instanceof Double) return true;
+
         try {
             Double.parseDouble(String.valueOf(obj));
             return true;
-        } catch (NullPointerException | NumberFormatException e){
+        } catch (NullPointerException | NumberFormatException e) {
             return false;
         }
     }
+
 
     public static int randInt(int min, int max, Random random) {
         return random.nextInt((max - min) + 1) + min;
     }
 
     public static int randInt(int min, int max) {
-        return randInt(min, max, random);
+        return randInt(min, max, DEFAULT_RANDOM);
+    }
+
+    public static long randLong(long min, long max, Random random) {
+        return random.nextLong((max - min) + 1L) + min;
+    }
+
+    public static long randLong(long min, long max) {
+        return randLong(min, max, DEFAULT_RANDOM);
     }
 
     public static double randDouble(double min, double max, Random random) {
@@ -48,8 +61,17 @@ public final class NumberUtils {
     }
 
     public static double randDouble(double min, double max) {
-        return randDouble(min, max, random);
+        return randDouble(min, max, DEFAULT_RANDOM);
     }
+
+    public static float randFloat(float min, float max, Random random) {
+        return min + (max - min) * random.nextFloat();
+    }
+
+    public static float randFloat(float min, float max) {
+        return randFloat(min, max, DEFAULT_RANDOM);
+    }
+
 
     public static int[] integersBetween(int start, int end) {
         return IntStream.range(start, end).toArray();
